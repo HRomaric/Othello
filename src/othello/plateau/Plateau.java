883b57698850bdec481dff31bf2a450f184b3de8
FRENZEL Loïc <frenzel4u@etu.univ-lorraine.fr>
2025-05-04 22:45:26 +0200

    Ajout du constructeur dans la classe Plateau et mise à jour de UML légèrement

--- src/othello/plateau/Plateau.java
+++ src/othello/plateau/Plateau.java
@@ -1,4 +1,11 @@
 package othello.plateau;
 
+import othello.outils.FabriquePlateau;
+
 public class Plateau {
+    private Case [][] plateauDeJeu;
+
+    public Plateau() {
+        plateauDeJeu = FabriquePlateau.getPlateauOthello();
+    }
 }
