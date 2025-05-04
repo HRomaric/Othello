f1c198396cfc08ffe3840beeeb12f20a7368a35e
FRENZEL Loïc <frenzel4u@etu.univ-lorraine.fr>
2025-05-04 22:43:46 +0200

    Mise à jour de la FabriquePlateau et de son UML

--- src/othello/outils/FabriquePlateau.java
+++ src/othello/outils/FabriquePlateau.java
@@ -1,4 +1,24 @@
 package othello.outils;
 
+import othello.plateau.Case;
+
 public class FabriquePlateau {
+    private FabriquePlateau() {
+    }
+
+    public static Case[][] getPlateauVide(){
+        return new Case[8][8] ;
+    }
+
+    public static Case[][] getPlateauOthello(){
+        Case[][] cases = getPlateauVide() ;
+        cases[3][3].setPionBlanc();
+        cases[3][4].setPionNoir();
+        cases[4][4].setPionBlanc();
+        cases[3][4].setPionNoir();
+
+        return cases ;
+    }
+
+
 }
