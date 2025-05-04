e0ee1e9757a17bdf593e84171fef29f0625066dc
FRENZEL Loïc <frenzel4u@etu.univ-lorraine.fr>
2025-05-04 22:43:16 +0200

    ajout du champ "plateau" à la classe Etat

--- src/othello/plateau/Etat.java
+++ src/othello/plateau/Etat.java
@@ -1,4 +1,26 @@
 package othello.plateau;
 
 public class Etat {
+    private Plateau plateau;
+
+    /**
+     * Constructeur Etat initial
+     */
+    public Etat(){
+    plateau = new Plateau();
+    }
+
+    /**
+     * Etat courant
+     * @param plateau
+     */
+    public Etat(Plateau plateau){
+        this.plateau = plateau;
+    }
+
+
+
+
+
+
 }
