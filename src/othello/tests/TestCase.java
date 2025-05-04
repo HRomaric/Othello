b313e8e336cc213cb84a8768d8d00035f32db97b
FRENZEL Loïc <frenzel4u@etu.univ-lorraine.fr>
2025-05-04 22:45:42 +0200

    Test du constructeur de Case

--- src/othello/tests/TestCase.java
+++ src/othello/tests/TestCase.java
@@ -1,4 +1,16 @@
 package othello.tests;
 
+
+import org.junit.jupiter.api.Test;
+import othello.plateau.Case;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestCase {
+
+    @Test
+    public void testConstructeur(){
+        Case c1  = new Case();
+        assertFalse(c1.isPionBlanc(),"Erreur par défaut une case n'est pas blanche");
+        assertFalse(c1.isPionNoir(),"Erreur par défaut une case n'est pas noire");
+    }
 }
