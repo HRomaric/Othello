afb59d43761a7a41be9c67b00c81cd806ab9e1c3
FRENZEL Loïc <frenzel4u@etu.univ-lorraine.fr>
2025-05-04 22:44:24 +0200

    Ajout des champs et des fonctions de bases mais pas encore terminé (Dans Case)

--- src/othello/plateau/Case.java
+++ src/othello/plateau/Case.java
@@ -1,4 +1,30 @@
 package othello.plateau;
 
 public class Case {
+    private boolean pionNoir ;
+    private boolean pionBlanc ;
+
+    public Case(){
+        pionNoir = false;
+        pionBlanc = false;
+    }
+
+    public void setPionNoir(){
+        if (pionBlanc) pionBlanc = false;
+        pionNoir = true;
+    }
+
+    public void setPionBlanc(){
+        if (pionNoir) pionNoir = false;
+        pionBlanc = true;
+    }
+
+
+    public boolean isPionNoir() {
+        return pionNoir;
+    }
+
+    public boolean isPionBlanc() {
+        return pionBlanc;
+    }
 }
