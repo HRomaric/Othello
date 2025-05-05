4c58c6212729e39f27f3fca241297e3f428c286c
Frenzel Loïc <frenzel4u@etu.univ-lorraine.fr>
2025-05-05 12:10:25 +0200

    Ajout de test du constructeur de Plateau

--- src/othello/tests/TestPlateau.java
+++ src/othello/tests/TestPlateau.java
@@ -1,4 +1,19 @@
 package othello.tests;
 
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import othello.plateau.Plateau;
+
 public class TestPlateau {
+
+    @Test
+    public void testConstructeurPlateau(){
+        Plateau p = new Plateau();
+        assertEquals(2, p.getNbPiontBlanc());
+        assertEquals(2, p.getNbPiontNoir());
+    }
+
+
+
 }
