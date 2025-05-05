a89cc3c3f4b2963dcfa110f8ad96354968ba55a4
henry559u <romaric.henry8@etu.univ-lorraine.fr>
2025-05-05 12:26:15 +0200

    Ajout de test Etat

--- src/othello/tests/TestEtat.java
+++ src/othello/tests/TestEtat.java
@@ -1,4 +1,28 @@
 package othello.tests;
 
+import org.junit.jupiter.api.Test;
+import othello.plateau.Etat;
+import othello.plateau.Plateau;
+
+import static org.junit.Assert.*;
+
 public class TestEtat {
+    @Test
+    public void testConstructeur1() {
+        Etat etat = new Etat();
+
+        assertTrue(etat.getEtatInitial());
+        assertFalse(etat.getEtatFinal());
+        assertNotNull(etat.getPlateau());
+    }
+
+    @Test
+    public void testConstructeur2() {
+        Plateau plateau = new Plateau();
+        Etat etat = new Etat(plateau);
+
+        assertFalse(etat.getEtatInitial());
+        assertFalse(etat.getEtatFinal());
+        assertNotNull(etat.getPlateau());
+    }
 }
