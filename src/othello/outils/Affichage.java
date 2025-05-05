736b47eaf8d9dc3321d2f8004773251f9493c5b6
henry559u <romaric.henry8@etu.univ-lorraine.fr>
2025-05-05 11:38:38 +0200

    Ajout d'un affichage fonctionnel et de son test

--- src/othello/outils/Affichage.java
+++ src/othello/outils/Affichage.java
@@ -1,4 +1,45 @@
 package othello.outils;
 
+import othello.plateau.Case;
+import othello.plateau.Plateau;
+
 public class Affichage {
+    public static void afficher(Plateau plateau) {
+        StringBuilder sb = new StringBuilder();
+        Case[][] p = plateau.getPlateauDeJeu();
+
+        sb.append("#############################################\n");
+        sb.append("######  1   2   3   4   5   6   7   8  ######\n");
+        sb.append("#############################################\n");
+
+        for(int i = 0; i < 8; i++) {
+            if (i == 0) {
+                sb.append("## 1 #");
+            }
+
+            for (int j = 0; j < 8; j++) {
+                if (j == 0) {
+                    sb.append(" ");
+                }
+                if (!p[i][j].isPionBlanc() && !p[i][j].isPionNoir()) {
+                    sb.append(" -  ");
+                } else if (p[i][j].isPionBlanc()) {
+                    sb.append(" B  ");
+                } else if (p[i][j].isPionNoir()) {
+                    sb.append(" N  ");
+                }
+            }
+
+            sb.append("# " + Integer.toString(i+1) + " ##\n");
+            if (i < 7){
+                sb.append("## " + Integer.toString(i+2) + " #");
+            }
+
+        }
+        sb.append("#############################################\n");
+        sb.append("######  1   2   3   4   5   6   7   8  ######\n");
+        sb.append("#############################################\n");
+
+        System.out.println(sb);
+    }
 }
--- src/othello/tests/TestAffichage.java
+++ src/othello/tests/TestAffichage.java
@@ -1,4 +1,14 @@
 package othello.tests;
 
+import org.junit.Test;
+import othello.outils.Affichage;
+import othello.plateau.Plateau;
+
 public class TestAffichage {
+    @Test
+    public void testAffichage() {
+        Plateau plateau = new Plateau();
+        Affichage affichage = new Affichage();
+        affichage.afficher(plateau);
+    }
 }
