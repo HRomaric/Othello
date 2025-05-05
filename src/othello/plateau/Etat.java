cde42c89e289ba25350968e47111058c935ce0de
henry559u <romaric.henry8@etu.univ-lorraine.fr>
2025-05-05 12:26:05 +0200

    Ajout commentaire

--- src/othello/plateau/Etat.java
+++ src/othello/plateau/Etat.java
@@ -1,14 +1,21 @@
 package othello.plateau;
 
 public class Etat {
-    private Plateau plateau;
+    private Plateau plateau; //Le plateau de jeu
+    private boolean etatInitial; //dit si on est l'état initial
+    private boolean etatFinal; //dit si on est un état final / échec
+
+
 
     /**
      * Constructeur Etat initial
      */
     public Etat(){
-    plateau = new Plateau();
+        plateau = new Plateau();
+        etatInitial = true;
+        etatFinal = false;
     }
+
 
     /**
      * Etat courant
@@ -16,10 +23,31 @@ public class Etat {
      */
     public Etat(Plateau plateau){
         this.plateau = plateau;
+        etatInitial = false;
+        etatFinal = false;
     }
 
-
+    /**
+     * Getteur du plateau de jeu
+     * @return plateau - Plateau
+     */
     public Plateau getPlateau() {
         return plateau;
+    }
+
+    /**
+     * Getteur du champ etatInitial
+     * @return etatInitial - boolean
+     */
+    public boolean getEtatInitial() {
+        return etatInitial;
+    }
+
+    /**
+     * Getteur du champ etatFinal
+     * @return etatFinal - boolean
+     */
+    public boolean getEtatFinal() {
+        return etatFinal;
     }
 }
