b41652c822230aeeba207d64d7ce33ef4c32ff53
henry559u <romaric.henry8@etu.univ-lorraine.fr>
2025-05-05 10:46:45 +0200

    ajout d'une fonction dans l'uml de Plateau classe plateau fct getPlateau
    + Ajout des fonctions décrites dans l'uml dans la classe Plateau.java

--- src/othello/plateau/Plateau.java
+++ src/othello/plateau/Plateau.java
@@ -4,8 +4,44 @@ import othello.outils.FabriquePlateau;
 
 public class Plateau {
     private Case [][] plateauDeJeu;
+    private int nbPiontBlanc;
+    private int nbPiontNoir;
+
+
 
     public Plateau() {
         plateauDeJeu = FabriquePlateau.getPlateauOthello();
+    }
+
+
+
+    public Case[][] getPlateauDeJeu() {
+        return plateauDeJeu;
+    }
+
+    public int getNbPiontBlanc(){
+        return nbPiontBlanc;
+    }
+
+    public int getNbPiontNoir(){
+        return nbPiontNoir;
+    }
+
+
+
+    public void majNbPiont(){
+        nbPiontBlanc = 0;
+        nbPiontNoir = 0;
+
+        for(int i = 0; i <= 8; i++){
+            for (int j = 0; j <= 8; j++){
+                if (plateauDeJeu[i][j].isPionBlanc()){
+                    nbPiontBlanc++;
+                }
+                if (plateauDeJeu[i][j].isPionNoir()){
+                    nbPiontNoir++;
+                }
+            }
+        }
     }
 }
