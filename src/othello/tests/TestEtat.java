360beb5c7c29b178a467a9b49a15ef752fa7b708
loicfrenzel <frenzel4u@etu.univ-lorraine.fr>
2025-05-19 00:09:32 +0200

    légère modifications des tests constructeurs

--- src/othello/tests/TestEtat.java
+++ src/othello/tests/TestEtat.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import othello.plateau.Etat;
 import othello.plateau.Plateau;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestEtat {
     @Test
@@ -12,17 +12,17 @@ public class TestEtat {
         Etat etat = new Etat();
 
         assertTrue(etat.getEtatInitial());
-        assertFalse(etat.getEtatFinal());
+        assertFalse(etat.estEtatFinal());
         assertNotNull(etat.getPlateau());
     }
 
     @Test
     public void testConstructeur2() {
         Plateau plateau = new Plateau();
-        Etat etat = new Etat(plateau);
+        Etat etat = new Etat(plateau, true);
 
         assertFalse(etat.getEtatInitial());
-        assertFalse(etat.getEtatFinal());
+        assertFalse(etat.estEtatFinal());
         assertNotNull(etat.getPlateau());
     }
 }
