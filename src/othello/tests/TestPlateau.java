7c73b41c96e6ee46953b4a5c3ff6e91b1d702d5f
loicfrenzel <frenzel4u@etu.univ-lorraine.fr>
2025-05-18 15:03:39 +0200

    Ajout de test sur la vérificationDirectionnel

--- src/othello/tests/TestPlateau.java
+++ src/othello/tests/TestPlateau.java
@@ -3,6 +3,9 @@ package othello.tests;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import othello.outils.Affichage;
+import othello.plateau.Etat;
 import othello.plateau.Plateau;
 
 public class TestPlateau {
@@ -13,6 +16,49 @@ public class TestPlateau {
         assertEquals(2, p.getNbPiontBlanc());
         assertEquals(2, p.getNbPiontNoir());
     }
+
+    @Test
+    public void testVerificationDirectionnelPourBlanc(){
+        Plateau p = new Plateau();
+
+        // horizontal vers la gauche
+        boolean test = p.verificationDirectionnel(3,5,0,-1,"Blanc"  );
+        assertTrue(test,"Erreur de verification directionnel vers la gauche car la case 3 5 à bien un voisin de même couleur plus loin vers la gauche  ");
+        // horizontale vers la droite
+        boolean test2 = p.verificationDirectionnel(3,5,0,1,"Blanc"  );
+        assertFalse(test2, "Erreur de vérification directionnel vers la droite car la case 3 5 n'a pas de voisin de même couleur plus loin vers la droite ");
+
+        // vertical vers le Bas
+
+        boolean test3 = p.verificationDirectionnel(5,3,1,0,"Blanc"  );
+        assertFalse(test3, "Erreur de vérification directionnel vers le bas , la case 5 3 n'a pas de voisin vertical plus bas de la même couleur");
+        // vertical vers le haut
+
+        boolean test4 = p.verificationDirectionnel(5,3,-1,0,"Blanc"  );
+        assertTrue(test4, "Erreur de vérification directionnel vers le haut, la case 5 3 a bien un voisin de couleur blanche plus haut ");
+    }
+
+
+    @Test
+    public void testVerificationDirectionnelPourNoir(){
+        Plateau p = new Plateau();
+        // horizontal vers la gauche
+        boolean test = p.verificationDirectionnel(3,2,0,-1,"Noir"  );
+        assertFalse(test, "Erreur de vérification directionnel vers la droite car la case 3 5 n'a pas de voisin de même couleur plus loin vers la gauche ");
+        // horizontale vers la droite
+        boolean test2 = p.verificationDirectionnel(3,2,0,1,"Noir"  );
+        assertTrue(test2,"Erreur de verification directionnel vers la gauche car la case 3 2 à bien un voisin de même couleur plus loin vers la droite  ");
+
+        // vertical vers le Bas
+        boolean test3 = p.verificationDirectionnel(2,3,1,0,"Noir"  );
+        assertTrue(test3, "Erreur de vérification directionnel vers le haut, la case 2 3 a bien un voisin de couleur blanche plus haut ");
+        // vertical vers le haut
+
+        boolean test4 = p.verificationDirectionnel(2,3,-1,0,"Noir"  );
+        assertFalse(test4, "Erreur de vérification directionnel vers le bas , la case 2 3 n'a pas de voisin vertical plus bas de la même couleur");
+
+    }
+
 
 
 
