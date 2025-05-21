e6f2fae7b35f1c6fed3d176bdf3098fd7276b43f
Frenzel Loïc <frenzel4u@etu.univ-lorraine.fr>
2025-05-21 13:06:41 +0200

    Ajout d'une classe Algo pas encore fonctionnel

---
+++ src/othello/outils/Algo.java
@@ -0,0 +1,31 @@
+package othello.outils;
+
+import othello.plateau.Etat;
+
+public class Algo {
+    private Algo(){}
+
+    private double evaluation(int c, Etat e){
+        if (e.estEtatFinal()){
+
+
+        }
+
+        return 3.5;
+    }
+
+
+    public static Etat minimax (Etat e , int c ){
+
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+}
