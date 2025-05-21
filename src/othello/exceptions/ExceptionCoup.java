f8cf6c7af4ffcd9394fb1d1b1bd3dd7bb6be9ad2
henry559u <romaric.henry8@etu.univ-lorraine.fr>
2025-05-21 10:38:56 +0200

    Création d'une exception pour avertir le joueur que son coup est impossible

---
+++ src/othello/exceptions/ExceptionCoup.java
@@ -0,0 +1,26 @@
+package othello.exceptions;
+
+import javafx.animation.PauseTransition;
+import javafx.scene.control.Alert;
+import javafx.util.Duration;
+
+public class ExceptionCoup extends Exception {
+    private String message;
+    public ExceptionCoup() {
+        super();
+        StringBuilder MessageErreur = new StringBuilder();
+        MessageErreur.append("\n\n").append("Vous devez fermez cette boite de dialogue pour revenir sur la creation du monde");
+        this.message = MessageErreur.toString();
+    }
+
+    public void afficherMsg(){
+        Alert dialog = new Alert(Alert.AlertType.ERROR);
+        dialog.setTitle("ERREUR COUP");
+        dialog.setHeaderText("Ce coup est impossible !");
+        dialog.setContentText(message);
+        PauseTransition pause = new PauseTransition(Duration.seconds(5));
+        pause.setOnFinished(event -> dialog.close());
+        pause.play();
+        dialog.showAndWait();
+    }
+}
