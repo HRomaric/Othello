ca524965bdc5b6f7df5b8822f2a0f0a8d6d4f0ad
henry559u <romaric.henry8@etu.univ-lorraine.fr>
2025-05-22 16:08:21 +0200

    Ajout des fonctions d'évaluations

--- src/othello/outils/Algo.java
+++ src/othello/outils/Algo.java
@@ -1,8 +1,10 @@
 package othello.outils;
 
+import othello.plateau.Case;
 import othello.plateau.Etat;
+import othello.plateau.Plateau;
 
-public class Algo {
+public class  Algo {
     private Algo(){}
 
     private double evaluation(int c, Etat e){
@@ -20,10 +22,62 @@ public class Algo {
         return null;
     }
 
+    public static int eval0(Etat e, int i){
+        switch (i){
+            case 0:{
+                return evalDiffPiont(e);
+            }
+            case 1: {
+                return evalPoidsPos(e);
+            }
+        }
+        return -1;
+    }
 
+    public static int evalDiffPiont(Etat e){
+        Plateau p = e.getPlateau();
+        if (e.joueurQuiJoue()){
+            return p.getNbPiontBlanc() - p.getNbPiontNoir();
+        }
+        else {
+            return p.getNbPiontNoir() - p.getNbPiontBlanc();
+        }
+    }
 
+    public static int evalPoidsPos(Etat e){
+        int [][] points = {
+                {100, 20 , 50 , 50 , 50 , 50 , 20 , 100},
+                {20 , 20 , 50 , 25 , 25 , 50 , 20 , 20 },
+                {50 , 25 , 25 , 5  , 5  , 25 , 25 , 50 },
+                {50 , 25 , 5  , 0  , 0  , 5  , 25 , 50 },
+                {50 , 25 , 5  , 0  , 0  , 5  , 25 , 50 },
+                {50 , 25 , 25 , 5  , 5  , 25 , 25 , 50 },
+                {20 , 20 , 50 , 25 , 25 , 50 , 20 , 20 },
+                {100, 20 , 50 , 50 , 50 , 50 , 20 , 100},
+        };
 
+        int scoreB = 0;
+        int scoreN = 0;
+        Case[][] p = e.getPlateau().getPlateauDeJeu();
 
+        for(int i = 0; i < 8; i++){
+            for (int j = 0; j < 8; j++){
+                if (p[i][j].isPionBlanc()){
+                    scoreB += points[i][j];
+                }
+                if (p[i][j].isPionNoir()){
+                    scoreN += points[i][j];
+                }
+            }
+        }
+
+        if (e.joueurQuiJoue()){
+            return scoreB;
+        }
+        else {
+            return scoreN;
+        }
+    }
 
 
 
