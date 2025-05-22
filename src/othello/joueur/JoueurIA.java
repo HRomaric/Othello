ee9a96d539f3f540378b30545669fc27cd403880
Frenzel Loïc <frenzel4u@etu.univ-lorraine.fr>
2025-05-22 13:53:23 +0200

    légère modif de la classe JoueurIA

--- src/othello/joueur/JoueurIA.java
+++ src/othello/joueur/JoueurIA.java
@@ -1,4 +1,18 @@
 package othello.joueur;
 
-public class JoueurIA {
+public class JoueurIA extends Joueur {
+
+    public JoueurIA(boolean couleur){
+        super(couleur);
+    }
+
+
+
+    /**
+     * Procédure qui fait jouer le joueur
+     */
+    @Override
+    public void jouer() {
+
+    }
 }
